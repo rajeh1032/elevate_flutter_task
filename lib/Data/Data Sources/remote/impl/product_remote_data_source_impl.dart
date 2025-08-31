b53a45f4991ae73c1d908df4f14ea779fd267e56
rajeh1032 <98366239+rajeh1032@users.noreply.github.com>
2025-08-31 03:42:55 +0300

    ssss

--- lib/Data/Data Sources/remote/impl/product_remote_data_source_impl.dart
+++ lib/Data/Data Sources/remote/impl/product_remote_data_source_impl.dart
@@ -8,9 +8,15 @@ import 'package:movies/core/api%20manager/api_endpoints.dart';
 import 'package:movies/core/api%20manager/api_manager.dart';
 import 'package:movies/core/utils/failures.dart';
 
+// ❌ import مش مستخدم (هيسبب Warning)
+import 'dart:math';
+
 @Injectable(as: ProductRemoteDataSource)
 class ProductRemoteDataSourceImpl implements ProductRemoteDataSource {
   ApiManager apiManager;
+
+  // ❌ متغير مش مستخدم (هيسبب Warning)
+  final String unusedVar = "test";
 
   ProductRemoteDataSourceImpl({required this.apiManager});
   @override
@@ -28,8 +34,9 @@ class ProductRemoteDataSourceImpl implements ProductRemoteDataSource {
             .map((e) => GetProductsDm.fromJson(e))
             .toList();
 
+        // ❌ مثال Error: رجعنا null بدل ما نرجع Either
         if (response.statusCode! >= 200 && response.statusCode! < 300) {
-          return Right(productList);
+          return Right(productList); // Analyzer + Compiler Error
         } else {
           return Left(ServerError(errorMessage: "Server error"));
         }
